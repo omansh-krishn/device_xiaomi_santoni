5bf7e991f6b2007cdb57657583726114a0b19335
Omansh Krishn <96343317+omansh-krishn@users.noreply.github.com>
2023-01-26 18:59:57 +0530

    changed to evolutionx

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,7 @@
-# AOSP
+# evolution
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_santoni.mk
+    $(LOCAL_DIR)/evolution_santoni.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_santoni-userdebug \
-    aosp_santoni-user \
-    aosp_santoni-eng
+    evolution_santoni-userdebug \
+    evolution_santoni-eng
